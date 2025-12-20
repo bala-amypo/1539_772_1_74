34cda3a33ae72c690809e0657ca9bf4fb0eebbc1
bala-amypo <bala@amypo.in>
2025-12-20 11:40:51 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/FraudRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/FraudRuleServiceImpl.java
@@ -1,3 +1,13 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.FraudRule;
+import com.example.demo.repository.FraudRuleRepository;
+import com.example.demo.service.FraudRuleService;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public class FraudRuleServiceImpl implements FraudRuleService {
 
