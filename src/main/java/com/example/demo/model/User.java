4db81800121b182c439aa79b626c6ac27f7427cf
bala-amypo <bala@amypo.in>
2025-12-22 05:05:55 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -25,7 +25,6 @@ public class User {
 
     @NotBlank(message = "Password is required")
     @Size(min = 6, message = "Password must be at least 6 characters")
-    @JsonIgnore
     private String password;
 
     private String role = "USER";
